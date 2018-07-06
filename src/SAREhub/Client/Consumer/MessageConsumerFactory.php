0404ab66bb62079b50845a97892d3ccce5c3d4ae
Jakub Cała <jakub.cala97@gmail.com>
2018-07-06 13:01:24 +0200

    Message consumer improvements (#2)
    * fixed PerWorkerMessageConsumerProvider
    
    * improvements with MessageConsumer
    
    * BasicMessageConsumerProvider moved to PHP Client as MessageConsumerProvider
    * PerWorkerMessageConsumerProvider moved to PHP Client as MessageConsumerProvider
    * MessageConsumerFactory implemented
    * composer.lock updated with PHP Client 0.5.1 version
    
    * Merge branch 'master' of https://github.com/SAREhub/php-di_client_bridge into message_consumer_improvements
    
    # Conflicts:
    #	src/SAREhub/Client/Consumer/PerWorkerMessageConsumerProvider.php
    
    * gitignore updated

---
+++ src/SAREhub/Client/Consumer/MessageConsumerFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+
+namespace SAREhub\Client\Consumer;
+
+
+use DI\Definition\Helper\FactoryDefinitionHelper;
+use function DI\factory;
+
+class MessageConsumerFactory
+{
+    public static function createExclusive(string $queueName, $processor): FactoryDefinitionHelper
+    {
+        return factory(MessageConsumerProvider::class)
+            ->parameter("consumerOptions", (new AmqpConsumerOptions())->setQueueName($queueName))
+            ->parameter("processor", $processor);
+    }
+
+    public static function create(
+        string $queueName,
+        int $workerId,
+        string $tagPattern,
+        $processor
+    ): FactoryDefinitionHelper {
+
+        $options = (new AmqpConsumerOptions())->setQueueName($queueName)->setTag(sprintf($tagPattern, $workerId));
+
+        return factory(MessageConsumerProvider::class)
+            ->parameter("consumerOptions", $options)
+            ->parameter("processor", $processor);
+    }
+}
