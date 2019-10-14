641a0ad4c3e6c112736111bd9536e49ef51875ac
Mararok <mararok@gmail.com>
2019-10-14 10:23:17 +0200

    add ContainerAwareIntegrationTestCase (#8)

---
+++ src/SAREhub/Client/DI/Test/ContainerAwareIntegrationTestCase.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace SAREhub\Client\DI\Test;
+
+use DI\ContainerBuilder;
+use PHPUnit\Framework\TestCase;
+use Psr\Container\ContainerInterface;
+use SAREhub\Commons\Logger\LoggerFactory;
+use SAREhub\Commons\Logger\NullLoggerFactory;
+use SAREhub\Commons\Secret\SecretValueProvider;
+use SAREhub\Commons\Secret\SimpleSecretValueProvider;
+use function DI\create;
+
+abstract class ContainerAwareIntegrationTestCase extends TestCase
+{
+    /**
+     * @var ContainerInterface
+     */
+    protected $container;
+
+    protected function setUp()
+    {
+        $this->container = $this->buildContainer();
+    }
+
+    protected function buildContainer(): ContainerInterface
+    {
+        $builder = new ContainerBuilder();
+        $this->addDefinitions($builder);
+        return $builder->build();
+    }
+
+    protected function addDefinitions(ContainerBuilder $builder): void
+    {
+        $builder->addDefinitions([
+            SecretValueProvider::class => create(SimpleSecretValueProvider::class),
+            LoggerFactory::class => create(NullLoggerFactory::class)
+        ]);
+    }
+}
